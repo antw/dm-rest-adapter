5ee30eae2cbe87aed3d55a25b221e718f162df25
Anthony Williams <hi@antw.me>
2011-04-07 02:20:04 +0100

    Remove old code from Adapter.

--- lib/dm-rest-adapter/adapter.rb
+++ lib/dm-rest-adapter/adapter.rb
@@ -177,43 +177,6 @@ module DataMapperRest
       query.options
     end
 
-    def record_from_rexml(entity_element, field_to_property)
-      record = {}
-
-      entity_element.elements.map do |element|
-        # TODO: push this to the per-property mix-in for this adapter
-        field = element.name.to_s.tr('-', '_')
-        next unless property = field_to_property[field]
-        record[field] = property.typecast(element.text)
-      end
-
-      record
-    end
-
-    def parse_resource(xml, model)
-      doc = REXML::Document::new(xml)
-
-      resource_name = resource_name(model)
-
-      unless entity_element = REXML::XPath.first(doc, "/#{resource_name}")
-        raise "No root element matching #{resource_name} in xml"
-      end
-
-      field_to_property = Hash[ model.properties(name).map { |p| [ p.field, p ] } ]
-      record_from_rexml(entity_element, field_to_property)
-    end
-
-    def parse_resources(xml, model)
-      doc = REXML::Document::new(xml)
-
-      field_to_property = Hash[ model.properties(name).map { |p| [ p.field, p ] } ]
-      resource_name     = resource_name(model)
-
-      doc.elements.collect("/#{collection_name(model)}/#{resource_name}") do |entity_element|
-        record_from_rexml(entity_element, field_to_property)
-      end
-    end
-
     # Updates a resource with attributes returned by the server.
     #
     # @param [DataMapper::Resource] resource
